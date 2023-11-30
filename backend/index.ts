8426314ee6cbfef26620d40adcd11f6c28e3ab19
Gabriel Cesario <gabrielmartins@mec.gov.br>
2023-11-30 19:26:16 -0300

    🎉 Initial Commit

---
+++ backend/index.ts
@@ -0,0 +1,34 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { StatusCodes } from "http-status-codes";
+
+dotenv.config();
+
+const app: Application = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors({}));
+
+const errorHandler: express.ErrorRequestHandler = (err, req, res, next) => {
+  if (err) {
+    res
+      .status(
+        err.statusCode ? err.statusCode : StatusCodes.INTERNAL_SERVER_ERROR
+      )
+      .json({ error: true, message: err.message });
+    return;
+  }
+  next();
+};
+
+app.get("", (req, res) => {
+  res.send("Olá mundo!");
+});
+
+app.use(express.json());
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}.`);
+});
